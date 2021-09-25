e0ed68c0452640e6cb845c55cffb9add06e41154
Tomotaka Sakuma <ktmtmks@gmail.com>
2021-09-25 09:56:45 +0900

    add: list-named-queries.

---
+++ list-named-query.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/service/athena"
+	"github.com/spf13/cobra"
+)
+
+func init() {
+	RootCmd.AddCommand(ListNamedQueriesCmd)
+}
+
+var ListNamedQueriesCmd = &cobra.Command{
+	Use:   "list-named-queries",
+	Short: "List named queries",
+	Run: func(cmd *cobra.Command, args []string) {
+		w := NewSession()
+		err := w.ListNamedQueries()
+		if err != nil {
+			log.Fatalf("%v", err)
+		}
+	},
+}
+
+func (sess *Session) ListNamedQueries() error {
+	r, err := sess.athenaClient.ListNamedQueries(sess.ctx, &athena.ListNamedQueriesInput{})
+	if err != nil {
+		return err
+	}
+	l := make([]*athena.GetNamedQueryOutput, 0)
+	for _, id := range r.NamedQueryIds {
+		r, err := sess.athenaClient.GetNamedQuery(sess.ctx, &athena.GetNamedQueryInput{NamedQueryId: aws.String(id)})
+		if err != nil {
+			return err
+		}
+		l = append(l, r)
+	}
+	b, err := json.MarshalIndent(l, "", "  ")
+	if err != nil {
+		return err
+	}
+	fmt.Printf("%v\n", string(b))
+	return nil
+}
